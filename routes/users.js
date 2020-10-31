6ca06043efaa68819b1aabe1a4c660957950a1a4
Cece <centrilam@gmail.com>
2020-10-30 21:42:03 -0700

    Passport JWT

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('../models/user');
 const passport = require('passport');
+const authenticate = require('../authenticate');
 
 const router = express.Router();
 
@@ -30,38 +31,10 @@ router.post('/signup', (req, res) => {
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
+    const token = authenticate.getToken({_id: req.user._id});
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.json({success: true, status: 'You are successfully logged in!'});
-});
-      
-        const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-        const username = auth[0];
-        const password = auth[1];
-      
-        User.findOne({username: username})
-        .then(user => {
-            if (!user) {
-                const err = new Error(`User ${username} does not exist!`);
-                err.status = 401;
-                return next(err);
-            } else if (user.password !== password) {
-                const err = new Error('Your password is incorrect!');
-                err.status = 401;
-                return next(err);
-            } else if (user.username === username && user.password === password) {
-                req.session.user = 'authenticated';
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end('You are authenticated!')
-            }
-        })
-        .catch(err => next(err));
-    } else {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('You are already authenticated!');
-    }
+    res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
 router.get('/logout', (req, res, next) => {
