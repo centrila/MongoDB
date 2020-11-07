fbf36c80c9e90ff528c2403a721d78e5a51739e1
Cece <centrilam@gmail.com>
2020-11-07 05:32:17 -0800

    oauth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '862088811289191',
+        clientSecret: '8848a66d1519d92a9002508b4b474644'
+    }
 }
